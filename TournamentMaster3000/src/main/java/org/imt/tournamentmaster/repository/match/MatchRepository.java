8a40585311563e6cc656dcaa5d7cbdf43d52b4e4
Julien SIMON <julien.simon@etu.univ-nantes.fr>
2026-01-18 20:24:04 +0100

    fix config et ajout date en BD

--- TournamentMaster3000/src/main/java/org/imt/tournamentmaster/repository/match/MatchRepository.java
+++ TournamentMaster3000/src/main/java/org/imt/tournamentmaster/repository/match/MatchRepository.java
@@ -4,6 +4,8 @@ import org.imt.tournamentmaster.model.match.Match;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MatchRepository extends CrudRepository<Match, Long> {
 Optional<Match> findFirstByOrderByDateMatchDesc();
